3d98a5fad821aa31b870fba07ead40ade1ed8774
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-30 12:30:52 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Module3HW3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            var class2 = new Class2();
+
+            Show(class2.Calc(class1.Pow, 2, 3)(3));
+        }
+
+        public static void Show(bool b)
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
